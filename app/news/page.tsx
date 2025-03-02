a26db7ce045f2d217dc2a8d4172ac5f0bcf69030
kanioh <kinokonosaka@gmail.com>
2025-03-02 14:01:22 +0900

    ニュースにSSRを適用

--- app/news/page.tsx
+++ app/news/page.tsx
@@ -4,6 +4,9 @@ import SearchField from "@/app/_components/SearchField";
 import { NEWS_LIST_LIMIT } from "@/app/_constants";
 import { getNewsList } from "@/app/_libs/microcms";
 
+// キャッシュの保持時間
+export const revalidate = 0;
+
 export default async function Page() {
     const { contents: news, totalCount } = await getNewsList({
         limit: NEWS_LIST_LIMIT,
